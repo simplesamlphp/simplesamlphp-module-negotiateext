7935b9e405d95d806c777122ba4b5b9d113770b3
Tim van Dijen <tvdijen@gmail.com>
2025-04-24 17:18:50 +0200

    bugfix: fix logout state reference

--- src/Auth/Source/Negotiate.php
+++ src/Auth/Source/Negotiate.php
@@ -341,7 +341,7 @@ class Negotiate extends \SimpleSAML\Auth\Source
     public function logout(array &$state): void
     {
         // get the source that was used to authenticate
-        $authId = $state['LogoutState']['negotiate:backend'];
+        $authId = $state['negotiate:backend'];
         Logger::debug('Negotiate - logout has the following authId: "' . $authId . '"');
 
         if ($authId === null) {
